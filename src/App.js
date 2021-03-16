802f5d46c6e0ef7dea1419793e84b759d0390ad5
Shawn <codingshawn@gmail.com>
2021-03-16 22:07:01 +0800

    Added handleLogin function to handle login after form submit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-  function handleLogin() {
-    
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch(exception) {
+
+    }
   }
-
-  return (
-    <>
-      <h1>Log in to application</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          Username{" "}
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          Password{" "}
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </>
-  );
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
+
+  if (user === null) {
+    return (
+      <>
+        <h1>Log in to application</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username{" "}
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            Password{" "}
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </>
+    );
+  }
 
   return (
     <div>
