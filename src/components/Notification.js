dac563139953357852e32448ff536afd5cada86a
Shawn <codingshawn@gmail.com>
2021-03-20 22:11:30 +0800

    Added notification component

---
+++ src/components/Notification.js
@@ -0,0 +1,19 @@
+function Notification({ text, error }) {
+  if (text === null) {
+    return null;
+  }
+  
+  let color = error ? "red" : "green";
+  let style = {
+    color: color,
+    fontWeight: 700,
+    border: `2px solid ${color}`,
+    borderRadius: "5px",
+    background: "#ddd",
+    padding: "5px",
+  };
+
+  return <div style={style}>{text}</div>;
+}
+
+export default Notification;
