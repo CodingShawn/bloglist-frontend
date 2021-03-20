61b183eacb9afd03508eccc7f2413574c0af7ede
Shawn <codingshawn@gmail.com>
2021-03-20 22:28:31 +0800

    Added notification message upon successful creation of blog

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import blogService from "../services/blogs";
+import Notification from "./Notification";
 
 function AddBlog() {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [url, setUrl] = useState("");
+  const [notification, setNotification] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   async function handleCreateBlog(event) {
     event.preventDefault();
@@ -14,13 +17,24 @@ function AddBlog() {
       url,
     };
     blogService.create(newBlog);
+    let notificationMessage = `A new blog \"${title}\" by ${author} was added`;
+    createNotification(notificationMessage);
     setTitle("");
     setAuthor("");
     setUrl("");
   }
+
+  function createNotification(notificationMessage) {
+    setNotification(notificationMessage);
+    setIsError(false);
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000);
+  }
   return (
     <>
       <h2>Create New Blog</h2>
+      <Notification text={notification} error={isError}/>
       <form onSubmit={handleCreateBlog}>
         <div>
           Title:{" "}
