5050ba8726ccf723bfcbc336aaae3af9692b51bd
Shawn <codingshawn@gmail.com>
2021-03-24 21:27:06 +0800

    Edited Blog component to allow toggling of all blog information

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from "react";
+import Togglable from "./Togglable";
 
-export default Blog
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 5,
+    paddingLeft: 2,
+    border: '1px black solid',
+    marginBottom: 5
+  }
+
+  return(
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author}
+    </div>
+    <Togglable buttonLabel="View">
+      <div>{blog.url}</div>
+      <div>likes: {blog.likes}<button>like</button></div>
+      <div>{blog.author}</div>
+    </Togglable>
+  </div>
+  )
+};
+
+export default Blog;
