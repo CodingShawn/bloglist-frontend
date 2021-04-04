fbbc3a5c3d7ee60b353789f484fe39169bbcac0e
Shawn <codingshawn@gmail.com>
2021-04-04 14:19:21 +0800

    Added rules to test both successful and unsuccessful login attempts (including that notification border is displayed red

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,15 +1,37 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", "http://localhost:3003/api/users", {
+      username: "Admin Test",
+      password: "password",
+      name: "Tester",
+    });
     cy.visit("http://localhost:3000");
   });
 
   it("Login form is shown", function () {
-    cy.get("h1")
-      .should("contain", "Log in to application");
+    cy.get("h1").should("contain", "Log in to application");
 
-    cy.get("form")
-      .get("button")
-      .should("contain", "Login")
+    cy.get("form").get("button").should("contain", "Login");
+  });
+
+  describe("Login", function () {
+    it("Succeeds with correct credentials", function () {
+      cy.get("#username").type("Admin Test");
+      cy.get("#password").type("password");
+      cy.get("#login-button").click();
+
+      cy.contains("Admin Test logged in");
+    });
+
+    it("Fails with wrong credentials", function () {
+      cy.get("#username").type("Admin Test");
+      cy.get("#password").type("wrong password");
+      cy.get("#login-button").click();
+
+      cy.get(".notification")
+        .contains("Wrong username or password")
+        .should("have.css", "border-color", "rgb(255, 0, 0)");
+    });
   });
 });
