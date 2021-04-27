76f4f008f076f8f6b8e499e9b985a08c6fed9b23
Shawn <codingshawn@gmail.com>
2021-04-27 22:47:18 +0800

    Added react devtools dependency and implement store and react devtools in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import notificationReducer from "./reducers/notificationReducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(notificationReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
