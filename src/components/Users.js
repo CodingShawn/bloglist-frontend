525e021f57565b3c68b306e8d69494513806ef52
Shawn <codingshawn@gmail.com>
2021-05-05 22:21:27 +0800

    Styled Users page

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Table } from "react-bootstrap";
 
 function Users() {
   const users = useSelector((state) => state.users);
 
   return (
     <section>
-      <h2>Users</h2>
-      {users &&
-        users.map((user) => (
-          <div key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.username} </Link>
-            {user.blogs.length}
-          </div>
-        ))}
+      <Table striped bordered size="sm">
+        <thead>
+          <tr>
+            <th>Users</th>
+            <th>Number of posted blogs</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users &&
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>
+                  <Link to={`/users/${user.id}`}>{user.username} </Link>
+                </td>
+                <td>{user.blogs.length}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
     </section>
   );
 }
